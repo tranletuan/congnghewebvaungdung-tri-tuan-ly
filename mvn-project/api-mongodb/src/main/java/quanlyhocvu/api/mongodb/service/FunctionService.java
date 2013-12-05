fcf9cf1c39d9525e3ab67ddf6de4a533b655433b
vanly.se05@gmail.com <vanly.se05@gmail.com@5a0f60ba-75b3-3dde-0b08-572cbee29771>
2013-12-05 14:19:14 +0000

    Add class Function Service to create some helper function for other class

---
+++ mvn-project/api-mongodb/src/main/java/quanlyhocvu/api/mongodb/service/FunctionService.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quanlyhocvu.api.mongodb.service;
+
+/**
+ *
+ * @author linhly
+ */
+public class FunctionService {
+    //format date from "day/month/year" to "month/day/year"
+    public static String formatStringDate(String date){
+        String res = "";
+        String[] arr = date.split("/");
+        res += arr[1] + "/" + arr[0]+ "/" + arr[2];
+        return res;
+        
+    }
+    
+}
