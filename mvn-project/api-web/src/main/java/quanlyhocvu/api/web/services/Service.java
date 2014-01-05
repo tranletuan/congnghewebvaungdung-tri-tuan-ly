3213c192e3a6bababa6695abd2ba50c9c74ffdae
VANLY.SE05@gmail.com <VANLY.SE05@gmail.com@5a0f60ba-75b3-3dde-0b08-572cbee29771>
2014-01-05 23:50:07 +0000

    POST SUCCESSFULLY

--- mvn-project/api-web/src/main/java/quanlyhocvu/api/web/services/Service.java
+++ mvn-project/api-web/src/main/java/quanlyhocvu/api/web/services/Service.java
@@ -6,6 +6,7 @@
 package quanlyhocvu.api.web.services;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,12 +19,15 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import quanlyhocvu.api.mongodb.DAO.MonHocDAO;
 import quanlyhocvu.api.mongodb.DTO.base.HocSinhDTO;
 import quanlyhocvu.api.mongodb.DTO.staff.GiaoVienDTO;
+import quanlyhocvu.api.mongodb.DTO.staff.KhoiLopDTO;
 import quanlyhocvu.api.mongodb.DTO.staff.MonHocDTO;
 import quanlyhocvu.api.mongodb.service.MongoService;
 import quanlyhocvu.api.web.controller.staff.ManagementSubjectController;
@@ -92,12 +96,40 @@ public class Service {
     }  
     
     @POST
-    @Path("/addNewStudent")
+    @Path("/putStudentByJSON")
     @Consumes(MediaType.APPLICATION_JSON)
-    public void addUser(HocSinhDTO hocsinh){
-         mongoService.insertStudent(hocsinh);
+    public void addUser(String hocsinh) throws JSONException{
+            System.out.println(hocsinh);
+            JSONObject hocSinhJSON = new JSONObject(hocsinh);
+            String maHocSinh = hocSinhJSON.getString("maHocSinh");
+            String tenHocSinh = hocSinhJSON.getString("tenHocSinh");
+            String diaChi = hocSinhJSON.getString("diaChi");
+            String ngaySinh = hocSinhJSON.getString("ngaySinh");
+            int gioiTinh = hocSinhJSON.getInt("gioiTinh");
+            String ngayNhapHoc = hocSinhJSON.getString("ngayNhapHoc");
+            Date ngaySinhDate = getDate(ngaySinh);
+            Date ngayNhapHocDate = getDate(ngayNhapHoc);
+            HocSinhDTO hs = new HocSinhDTO();
+            hs.setmaHocSinh(maHocSinh);
+            hs.sethoTen(tenHocSinh);
+            hs.setdiaChi(diaChi);
+            hs.setgioiTinh(gioiTinh);
+            hs.setngaySinh(ngaySinhDate);
+            hs.setngayNhapHoc(ngayNhapHocDate);
+            KhoiLopDTO khoiLopHienTai = mongoService.getKhoiLopByName("6");
+            hs.setKhoiLopHienTai(khoiLopHienTai);            
+            System.out.println(hocSinhJSON.toString());
+            System.out.println(ngaySinh);
+            System.out.println(hs);
+            mongoService.insertStudent(hs);
     }
     
+    public Date getDate(String date){        
+        int year = Integer.parseInt(date.substring(0, 4)) - 1900;
+        int month = Integer.parseInt(date.substring(5, 7));
+        int day = Integer.parseInt(date.substring(8, 10));
+        return new Date(year, month, day);
+    }
         
 
     @GET
